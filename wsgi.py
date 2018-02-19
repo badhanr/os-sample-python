3446123ac22d9aee9a01401c72f633b6c7c84f25
badhanr <badhan.rahul@outlook.com>
2018-02-19 14:55:02 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -5,5 +5,5 @@ cnx = mysql.connector.connect(user='userRTL', password='eNU1gYbC1EYLe6gN',
                               host='127.0.0.1',
                               database='sampledb',
                               use_pure=False)
-cnx.close()
+
 
