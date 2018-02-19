30e91d82e4a0ea3a37fbec9bbc281822f3eb7142
badhanr <badhan.rahul@outlook.com>
2018-02-19 23:47:20 +0530

    Update db.sql

--- db.sql
+++ db.sql
@@ -4,3 +4,17 @@ create table emp
     
 );
 INSERT INTO emp(name)VALUES ('Mac')
+sql = "SELECT * FROM EMPLOYEE
+sql = "SELECT * FROM EMPLOYEE \
+       WHERE INCOME > '%d'" % (1000)
+try:
+   # Execute the SQL command
+   cursor.execute(sql)
+   # Fetch all the rows in a list of lists.
+   results = cursor.fetchall()
+   for row in results:
+      fname = row[0]
+      # Now print fetched result
+      print "fname=%s"(fname)
+except:
+   print "Error: unable to fecth data"
