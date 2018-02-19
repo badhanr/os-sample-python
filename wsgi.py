f46c24bcb7df5a866f7b3236529b13ee26161269
badhanr <badhan.rahul@outlook.com>
2018-02-19 15:20:06 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,18 @@
-import pymysql
-import pymysql.cursors
-import mysql.connector
-conn= pymysql.connect(host='mysql://172.30.115.81:3306',user='userRTL',password='eNU1gYbC1EYLe6gN',db='sampledb',charset='utf8mb4',cursorclass=pymysql.cursors.DictCursor)
-a=conn.cursor()
-sql='CREATE TABLE `users` (`id` int(11) NOT NULL AUTO_INCREMENT,`email` varchar(255) NOT NULL,`password` varchar(255) NOT NULL,PRIMARY KEY (`id`)) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;'
-a.execute(sql)
+#import pymysql
+#import pymysql.cursors
+#import mysql.connector
+#conn= pymysql.connect(host='mysql://172.30.115.81:3306',user='userRTL',password='eNU1gYbC1EYLe6gN',db='sampledb',charset='utf8mb4',cursorclass=pymysql.cursors.DictCursor)
+#a=conn.cursor()
+#sql='CREATE TABLE `users` (`id` int(11) NOT NULL AUTO_INCREMENT,`email` varchar(255) NOT NULL,`password` varchar(255) NOT NULL,PRIMARY KEY (`id`)) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;'
+#a.execute(sql)
+import MySQLdb
+
+# Open database connection
+db = MySQLdb.connect("localhost","userRTL","eNU1gYbC1EYLe6gN","sampledb" )
+
+# prepare a cursor object using cursor() method
+cursor = db.cursor()
+
+# execute SQL query using execute() method.
+cursor.execute("SELECT VERSION()")
+
