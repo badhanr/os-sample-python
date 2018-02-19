7fa1d893f210d6d6664000de04486ab8ffb5133d
badhanr <badhan.rahul@outlook.com>
2018-02-19 15:09:23 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,12 +1,7 @@
 import pymysql
 import pymysql.cursors
 import mysql.connector
-conn= pymysql.connect(host='localhost',user='userRTL',password='eNU1gYbC1EYLe6gN',db='sampledb',charset='utf8mb4',cursorclass=pymysql.cursors.DictCursor)
+conn= pymysql.connect(host='127.0.0.1',user='userRTL',password='eNU1gYbC1EYLe6gN',db='sampledb',charset='utf8mb4',cursorclass=pymysql.cursors.DictCursor)
 a=conn.cursor()
-#cnx = mysql.connector.connect(user='userRTL', password='eNU1gYbC1EYLe6gN',
-                             # host='127.0.0.1',
-                              #database='sampledb',
-                              #use_pure=False)
-
 sql='CREATE TABLE `users` (`id` int(11) NOT NULL AUTO_INCREMENT,`email` varchar(255) NOT NULL,`password` varchar(255) NOT NULL,PRIMARY KEY (`id`)) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;'
 a.execute(sql)
