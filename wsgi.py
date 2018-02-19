0485e291cd0b1af1c82ecfe4bb775b37937e41cb
badhanr <badhan.rahul@outlook.com>
2018-02-19 17:03:32 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -5,7 +5,7 @@ application = Flask(__name__)
 @application.route("/")
 def hello():
     # Open database connection
-    db = MySQLdb.connect("mysql://172.30.115.81:3306","userRTL","eNU1gYbC1EYLe6gN","sampledb" )
+    db = MySQLdb.connect("sampledb" )
     # prepare a cursor object using cursor() method
     cursor = db.cursor()
     # execute SQL query using execute() method.
