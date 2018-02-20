8a710ff518bb495bca79536eeda290ed542d61a7
badhanr <badhan.rahul@outlook.com>
2018-02-20 18:39:50 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,16 +1,16 @@
 from flask import Flask
-import MySQLdb
+#import MySQLdb
 from dbhelper import DBHelper
-application = Flask(__name__)
+application = Flask(__name__)c
 
 @application.route("/")
 def hello():
     # Open database connection
-    db = MySQLdb.connect(host="172.30.115.81", port=3306, user="userRTL", passwd="eNU1gYbC1EYLe6gN", db="sampledb" )
+    #db = MySQLdb.connect(host="172.30.115.81", port=3306, user="userRTL", passwd="eNU1gYbC1EYLe6gN", db="sampledb" )
     # prepare a cursor object using cursor() method
-    cursor = db.cursor()
+    #cursor = db.cursor()
     # execute SQL query using execute() method.
-    cursor.execute("SELECT VERSION()")
+    #cursor.execute("SELECT VERSION()")
     DBh=DBHelper()
     DBh.setup()
     
