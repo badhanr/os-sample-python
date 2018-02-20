cb9ea404becaa9ff12a31db30c5420569190d519
badhanr <badhan.rahul@outlook.com>
2018-02-20 10:53:28 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -10,16 +10,16 @@ def hello():
     cursor = db.cursor()
     # execute SQL query using execute() method.
     cursor.execute("SELECT VERSION()")
-    sql = """CREATE TABLE EMPLOYEE (
-         FIRST_NAME  CHAR(20) NOT NULL,
-         LAST_NAME  CHAR(20),
-         AGE INT,  
-         SEX CHAR(1),
-         INCOME FLOAT )"""
-    cursor.execute(sql)
+    #sql = """CREATE TABLE EMPLOYEE (
+         #FIRST_NAME  CHAR(20) NOT NULL,
+         #LAST_NAME  CHAR(20),
+         #AGE INT,  
+         #SEX CHAR(1),
+         #INCOME FLOAT )"""
+    #cursor.execute(sql)
     sql = """INSERT INTO EMPLOYEE(FIRST_NAME,
          LAST_NAME, AGE, SEX, INCOME)
-         VALUES ('Mac', 'Mohan', 20, 'M', 2000)"""
+         VALUES ('Madan', 'Mac', 20, 'M', 5000)"""
     cursor.execute(sql)
     sql = "SELECT * FROM EMPLOYEE \
        WHERE INCOME > '%d'" % (1000)
