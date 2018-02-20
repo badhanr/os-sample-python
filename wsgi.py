1ceac2464eadd597b95e4377622cc52c5046f2e4
badhanr <badhan.rahul@outlook.com>
2018-02-20 18:54:03 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -12,34 +12,7 @@ def hello():
     # execute SQL query using execute() method.
     #cursor.execute("SELECT VERSION()")
     DBh=DBHelper()
-    #DBh.setup()
-    
-    #sql = """CREATE TABLE EMPLOYEE (
-         #FIRST_NAME  CHAR(20) NOT NULL,
-         #LAST_NAME  CHAR(20),
-         #AGE INT,  
-         #SEX CHAR(1),
-         #INCOME FLOAT )"""
-    #cursor.execute(sql)
-    #sql = """INSERT INTO EMPLOYEE(FIRST_NAME,
-         #LAST_NAME, AGE, SEX, INCOME)
-         #VALUES ('drakshya', 'Mac', 20, 'F', 10000)"""
-    #cursor.execute(sql)
-    #db.commit()
-    #sql = "SELECT FIRST_NAME FROM EMPLOYEE WHERE INCOME =(?)"
-    #args = ('100',)
-    #cursor.execute(sql,args)
-    #results = cursor.fetchall()
-    #for row in results:
-      #fname = row[0]
-      #lname = row[1]
-      #age = row[2]
-      #sex = row[3]
-      #income = row[4]
-      # Now print fetched result
-      #print "fname=%s,lname=%s,age=%d,sex=%s,income=%d" % \
-             #(fname, lname, age, sex, income )
-    #return(results)
+    DBh.setup()
     return("test")
 if __name__ == "__main__":
     application.debug = True
