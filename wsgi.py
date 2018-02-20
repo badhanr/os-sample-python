fb0e8fe47e75022fe6f8d4aa6c1ab94cc947451e
badhanr <badhan.rahul@outlook.com>
2018-02-20 10:45:52 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -10,6 +10,10 @@ def hello():
     cursor = db.cursor()
     # execute SQL query using execute() method.
     cursor.execute("SELECT VERSION()")
+    sql = """CREATE TABLE EMPLOYEE (FIRST_NAME  CHAR(20) NOT NULL)"""
+    cursor.execute(sql)
+    sql = "INSERT INTO EMPLOYEE(FIRST_NAME)VALUES ('%s')('Mac')"
+    cursor.execute(sql)
     return("test")
     
 if __name__ == "__main__":
