c4c22c07c893259084717530b3148098738083ef
badhanr <badhan.rahul@outlook.com>
2018-02-21 17:10:48 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -8,12 +8,15 @@ def hello():
     DBh=DBHelper()
     DBh.setup()
     DBh.add_item("abc","def")
+    
     #DBh.delete_item("abc","def")
     res1=DBh.get_items("def")
     print(res1)
     #DBh.delete_chat("def")
     #DBh.delete_case("1234","def")
     DBh.add_case_subject("1234","abc", "cde", "tesr", "test", "7/28/2018")
+    DBh.update_case_detail("yuc", "abc","21/2/2018","1234","1234")
+     DBh.update_case_phn_loc("7894455612","Pune", "abc","21/2/2018","Elon","1234")
     res2=DBh.get_case_subject("1234" ,"abc","21/2/2018")
     print(res2)
     res3=DBh.get_case_department("1234","abc")
@@ -21,8 +24,8 @@ def hello():
     res4=DBh.get_case_whd_ticket_id("1234","abc")
     print(res4)
     #DBh.delete_invalid_cases("hjju")
-    DBh.update_case_detail("yuc", "abc","21/2/2018","1234","1234")
-    DBh.update_case_phn_loc("7894455612","Pune", "abc","21/2/2018","Elon","1234")
+    
+   
     DBh.update_whd_ticket_id("1234569", "Elon","21/2/2018","1234")    
     res5=DBh.get_pending_case("abch")
     print(res5)
