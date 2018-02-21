d15f51c79b4f93bec288ea38ef1cb0df434dc186
badhanr <badhan.rahul@outlook.com>
2018-02-21 16:25:56 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -15,7 +15,7 @@ def hello():
     DBh.add_case_subject("1234","abc", "cde", "tesr", "test", "7/28/2018")
     DBh.get_case_subject("1234" ,"abc","21/2/2018")
     DBh.get_case_department("1234","abc")
-    DBh.get_case_whd_ticket_id("abc")
+    DBh.get_case_whd_ticket_id("1234","abc")
     DBh.delete_invalid_cases("hjju")
     DBh.update_case_detail("yuc", "abc","21/2/2018","1234","1234")
     
