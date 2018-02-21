317bb414a75c8f4837cded2c48fc7d71e683c731
badhanr <badhan.rahul@outlook.com>
2018-02-21 11:56:57 +0530

    Update dbhelper.py

--- dbhelper.py
+++ dbhelper.py
@@ -3,8 +3,8 @@ class DBHelper:
     
     def __init__(self, dbname="sampledb"):
         #self.dbname = dbname
-        db=MySQLdb.connect(host="172.30.115.81", port = 3306, user = "userRTL", passwd = "eNU1gYbC1EYLe6gN", db = "sampledb" )
-        self.conn = db.cursor()
+        self.conn=MySQLdb.connect(host="172.30.115.81", port = 3306, user = "userRTL", passwd = "eNU1gYbC1EYLe6gN", db = "sampledb" )
+        #self.conn = db.cursor()
 
 
     def setup(self):
